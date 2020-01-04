7093f05351c9ab6ac677157c1d833a8fca72100f
Michael Bestas <mkbestas@lineageos.org>
2020-01-04 15:40:59 +0000

    msm8953-common: Disable DPM features
    •Stops it from crashing, most devices have it disabled anyway.

--- prop.mk
+++ prop.mk
@@ -52,7 +52,7 @@ ro.qualcomm.bt.hci_transport=smd
 # Cne/Dpm
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.vendor.cne.feature=1 \
-persist.dpm.feature=1
+persist.vendor.dpm.feature=0
 
 # Coresight
 PRODUCT_PROPERTY_OVERRIDES += \
