87390016324c4375fea2bc1042c6edaaa9fdba4e
mahajant99 <mahajant99@gmail.com>
2020-03-28 16:27:37 +0530

    sakura: Update Blobs From Daisy Q * Updated From daisy-user-10-QKQ1.191002.002-V11.0.2.0.QDLMIXM-release-keys * Updated Camera Blobs * Updated Consumer IR Blobs * Updated Fingerprint Blobs
    Signed-off-by: mahajant99 <mahajant99@gmail.com>

--- prop.mk
+++ prop.mk
@@ -241,7 +241,7 @@ wifi.interface=wlan0
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
 vendor.camera.aux.packageblacklist=com.discord \
-persist.camera.HAL3.enabled=1 \
+persist.vendor.camera.HAL3.enabled=1 \
 vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera2,com.google.android.GoogleCamera \
 vendor.camera.aux.packagelist2=com.google.android.GoogleCameraWide,com.dual.GCam,com.Wide.GCam,com.Tele.GCam \
 vendor.camera.hal1.packagelist=com.whatsapp \
